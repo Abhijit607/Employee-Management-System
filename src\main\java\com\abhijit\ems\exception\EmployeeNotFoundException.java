89677ec188ebcf07aaed6c9dc36a66faa2c52625
Abhijit607 <abhijitap9369@gmail.com>
2026-06-02 14:08:24 +0530

    Remove incorrect path src\main\java\com\abhijit\ems\exception\EmployeeNotFoundException.java

--- src\main\java\com\abhijit\ems\exception\EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.abhijit.ems.exception;
-
-public class EmployeeNotFoundException extends RuntimeException {
-    public EmployeeNotFoundException(int employeeId) {
-        super("Employee not found with id: " + employeeId);
-    }
-}
